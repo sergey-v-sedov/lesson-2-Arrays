c87551bc53ad7329440e459b8b81b2e984f920a8
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-06-01 21:19:26 +0300

    Посчитать кол-во кораблей на поле для "Морского боя"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,29 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ;
+        byte[][] field = new byte[][]{
+                {0,1,0,0,0,0,0,0,0,0},
+                {0,0,1,0,0,0,1,1,1,1},
+                {0,0,1,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,0},
+                {0,0,0,1,1,1,0,0,0,0},
+                {1,1,0,0,0,0,0,0,0,0},
+                {0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,1,0,0,0,0,1}
+        }; // 7
+
+        byte count = 0;
+
+        for(int i=0; i< field.length; i++) {
+            for(int j=0; j< field[i].length; j++) {
+                byte cell = field[i][j];
+                if(cell == 1 && (j==0 || field[i][j-1] == 0) && (i==0 || field[i-1][j] == 0)) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
